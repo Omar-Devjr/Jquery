55c1f5a1bb0d4ad2c117f9010f59ac26c00a2c69
Omar-Devjr <ungober@gmail.com>
2020-02-19 21:58:17 -0600

    :octocat: Sixtanxix Jquery

---
+++ js/main.js
@@ -0,0 +1,17 @@
+ 
+/*let button = document.getElementById('flex-btn')
+
+button.addEventListener('click', () => {
+document.getElementById('square-wrapper')
+.classList.add('d-flex')
+})*/
+
+
+/*sujeto , vervo, predicado*/
+
+/*$("square-wrappe").addClass("d-flex")*/
+
+$('#flex-btn').on('click',() => {
+$('#square-wrapper').addClass("d-flex")
+
+})
